1ee3d2a59aac9ab5d57bb9f31c5d6c30fe80eaec
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-04 09:51:11 -0300

    seeders com estado e cidade, migrations pets e users prontos

---
+++ database/migrations/2023_11_03_111159_create_pets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('age');
+            $table->string('species');
+            $table->string('breeds');
+            $table->foreignId('users_id')->constrained()->onDelete('cascade');
+            $table->foreignId('cities_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pets');
+    }
+};
