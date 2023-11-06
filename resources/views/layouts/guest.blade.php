a935583f210d89ec4175367fdb06af8587eb89b9
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-06 17:11:46 -0300

    reorganizando guest

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,27 +6,35 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name') }}</title>
+        <link rel="icon" type="image/x-icon" href="{{ asset("footprint-96.png")}}">
         <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     
     </head>
     <body class="body-guest">
         <header>
-            <img src="public/build/images/header-bg.svg" alt="" class="img-header-esquerdo">
+            <img src="{{ asset('images/header-bg.svg') }}" alt="" class="header-guest-left">
         </header>
-        <div class="main-guest">
-            <main>
+        <div class="container-guest">
+            <main class="main-guest">
                 <section>
-                    <article class="frases-guest">
-                        <img src="public/build/imagens/Logo.png" class="logo">
-                        <h2 class="h2-guest">Boas Vindas</h2>
-                        <p class="p-guest">Adotar pode mudar uma vida. Que tal buscar<br>
+                    <div class="logo">
+                        <figure>
+                            <img src="{{ asset('images/logo.png') }}">
+                        </figure>
+                    </div>
+                    <div class="phrases-guest">
+                        <h2>Boas Vindas</h2>
+                        <p class="p-adoptme">Adotar pode mudar uma vida. Que tal buscar<br>
                         seu novo melhor amigo hoje? Vem com a gente!</p>
-                    </article>
-            
+                    </div>
                     <div >
                         {{ $slot }}
                     </div>
+                    <img src="{{ asset('images/pets.svg') }}" alt="" class="footer-pets">
                 </section>
+                <aside>
+                    <img src="{{ asset('images/body-bg.svg') }}" alt="" class="header-guest-right">
+                </aside>
             </main>
             <footer>
                 <p>2023 - Desenvolvido por <a href="https://github.com/Brunammsa" target='_blank' rel="noreferrer">BMMSA</a></p>
