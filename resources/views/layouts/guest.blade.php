6238c911cb0302786e95bc9ce3b370eb2f2e0471
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-07 17:47:50 -0300

    ajustes layout guest e login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,34 +11,32 @@
     
     </head>
     <body class="body-guest">
-        <header>
-            <img src="{{ asset('images/header-bg.svg') }}" alt="" class="header-guest-left">
-        </header>
-        <div>
-            <main class="main-guest">
-                <section>
-                    <figure class="logo">
-                        <img src="{{ asset('images/logo.png') }}">
-                    </figure>
-                    <article class="welcome-guest">
-                        <h2>Boas Vindas</h2>
-                        <p class="p-adoptme">Adotar pode mudar uma vida. Que tal buscar<br>
-                        seu novo melhor amigo hoje? Vem com a gente!</p>
-                        <div >
-                            {{ $slot }}
-                        </div>
-                    </article>
-                </section>
-                <figure>
-                    <img src="{{ asset('images/body-bg.svg') }}" alt="" class="header-guest-right">
-                </figure>
-            </main>
+        <main class="main-guest">
             <figure>
-                <img src="{{ asset('images/pets.svg') }}" alt="" class="footer-pets">
+                <img src="{{ asset('images/header-bg.svg') }}" alt="" class="header-guest-left">
             </figure>
-            <footer>
-                <p>2023 - Desenvolvido por <a href="https://github.com/Brunammsa" target='_blank' rel="noreferrer">BMMSA</a></p>
-            </footer>
-        </div>
+            <section class="section-guest">
+                <figure class="logo">
+                    <img src="{{ asset('images/logo.png') }}">
+                </figure>
+                <article class="welcome-guest">
+                    <h2>Boas Vindas!</h2>
+                    <p class="p-adoptme">Adotar pode mudar uma vida. Que tal buscar<br>
+                    seu novo melhor amigo hoje? Vem com a gente!</p>
+                    <div >
+                        {{ $slot }}
+                    </div>
+                </article>
+            </section>
+            <figure>
+                <img src="{{ asset('images/body-bg.svg') }}" alt="" class="header-guest-right">
+            </figure>
+        </main>
+        <figure>
+            <img src="{{ asset('images/pets.svg') }}" alt="" class="footer-pets">
+        </figure>
+        <footer>
+            <p>2023 - Desenvolvido por <a href="https://github.com/Brunammsa" target='_blank' rel="noreferrer">BMMSA</a></p>
+        </footer>
     </body>
 </html>
