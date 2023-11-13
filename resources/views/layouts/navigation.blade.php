877a2d12cdd2d7ebc1014ff412525aeb4ed68db5
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-13 17:13:44 -0300

    adicionando logo no app

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -8,7 +8,13 @@
         <div class="flex justify-between h-16">
             
             <div class="flex">
-
+                <!-- Logo -->
+                <div class="logo-app">
+                    <a href="{{ route('dashboard') }}">
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                    </a>
+                </div>
+                
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')">
