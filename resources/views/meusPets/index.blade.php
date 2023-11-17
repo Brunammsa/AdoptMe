a71e948e55c6a80899b08ed926a08a674e47fea4
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-17 16:07:56 -0300

    botões de edit e destroy ok

--- resources/views/meusPets/index.blade.php
+++ resources/views/meusPets/index.blade.php
@@ -13,8 +13,18 @@
                         <li class="container-pets">
                             {{$pet->name}}
                             <div class="icones-att-del">
-                                <img src="{{asset('images/edit.png')}}" alt="">
-                                <img src="{{asset('images/delete.png')}}" alt="">
+                                <a href="{{route('meusPets.edit', $pet->id)}}">
+                                    <img src="{{asset('images/edit.png')}}" alt="editar pet">
+                                </a>
+
+                                <form action="{{route('meusPets.destroy', $pet->id)}}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+
+                                    <button>
+                                        <img src="{{asset('images/delete.png')}}" alt="deletar pet">
+                                    </button>
+                                </form>
                             </div>
                         </li>
                         <hr>
@@ -24,3 +34,4 @@
         </div>
     </section>
 </x-app-layout>
+
