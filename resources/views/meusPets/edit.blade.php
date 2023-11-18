b5bfb5fd5752405bd150292da0d9faecd1aa8d5b
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-18 16:41:43 -0300

    passando os parâmetros para o form

--- resources/views/meusPets/edit.blade.php
+++ resources/views/meusPets/edit.blade.php
@@ -1,9 +1,14 @@
 <x-app-layout>
     <section class="section-create">
         <div class="cabecalho-novoPet">
-            <h1 class=" mb-4 mt-20">Edição do pet '{{$pets->name}}':</h1>
+            <h1 class=" mb-4 mt-20">Edição do pet  <strong>'{{$pets->name}}'</strong></h1>
             <img src="{{ asset('images/Patas.png') }}" alt="imagem patas" class="patas">
         </div>
-        <x-form :action="route('meusPets.store')" :nome="$pets->name"></x-form>
+        <x-form :action="route('meusPets.update', $pets->id)" 
+            :name="$pets->name"
+            :age="$pets->age"
+            :size="$pets->size"
+            :description="$pets->description"
+            ></x-form>
     </section>
 </x-app-layout>
