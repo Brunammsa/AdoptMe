55f6f70683322c4f09e2ae8b996876842cfe4a9b
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-20 16:49:23 -0300

    criando usuário api automático na seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory()->create([
+            'name' => 'Api User',
+            'email' => 'user@api',
+            'password' => 'megaman2',
+        ]);
 
         $this->call([
             StatesTableSeeder::class,
