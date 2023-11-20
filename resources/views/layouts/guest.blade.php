1bc6a8a5865e0e02061ca9a448f861b53b5e1333
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-20 16:50:03 -0300

    coloquei o link da font poppis

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -8,6 +8,8 @@
         <title>{{ config('app.name') }}</title>
         <link rel="icon" type="image/x-icon" href="{{ asset("footprint-96.png")}}">
         <link href="{{ asset('css/styleGuest.css') }}" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&display=swap" rel="stylesheet">
     
     </head>
     <body>
