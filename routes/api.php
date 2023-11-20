45d6d1f12b71bdfdead172f3edaf6fd6eaed0a04
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-20 17:08:24 -0300

    corrigindo erro de namespace e use

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ApiStateController;
+use App\Http\Controllers\Api\ApiStateController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
