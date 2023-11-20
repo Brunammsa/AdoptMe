84fef5d2e882c980cec718dfb44d029dcb7ba310
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-20 15:01:30 -0300

    implementando @props com váriaveis que serão utilizadas e recuperando com old (ainda não pegando)

--- resources/views/meusPets/edit.blade.php
+++ resources/views/meusPets/edit.blade.php
@@ -1,14 +1,20 @@
+@props([
+    'name' => $pets->name,
+    'age' => $pets->age,
+    'description' => $pets->description,
+    'id' => $pets->id
+])
+
 <x-app-layout>
     <section class="section-create">
         <div class="cabecalho-novoPet">
-            <h1 class=" mb-4 mt-20">Edição do pet  <strong>'{{$pets->name}}'</strong></h1>
+            <h1>Edição do pet  <strong>'{{$name}}'</strong></h1>
             <img src="{{ asset('images/Patas.png') }}" alt="imagem patas" class="patas">
         </div>
-        <x-form :action="route('meusPets.update', $pets->id)" 
-            :name="$pets->name"
-            :age="$pets->age"
-            :size="$pets->size"
-            :description="$pets->description"
+        <x-form :action="route('meusPets.update', $id)" 
+            :name="old('name')"
+            :age="old('age')"
+            :description="old('description')"
             ></x-form>
     </section>
 </x-app-layout>
