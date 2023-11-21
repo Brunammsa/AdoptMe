ef9cdc7ca8d330453deb5fe18cd162cfef064191
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-21 16:24:36 -0300

    estruturando text-input

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,6 @@
-@props(['disabled' => false])
+@props(['type' => 'text', 'name'])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+<input  
+    type="{{ $type }}"
+    name="{{ $name }}"
+    {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
