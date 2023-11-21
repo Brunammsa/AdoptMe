7133d8458c7903902e595dd99bdd952ff71b6abc
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-21 07:45:30 -0300

    dentro do @props() retirei 'size'e coloquei []

--- resources/views/components/input-select.blade.php
+++ resources/views/components/input-select.blade.php
@@ -1,4 +1,8 @@
-<select  {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+@props([])
+
+<select  {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm', 
+        'name' => 'size', 
+        'id'=> 'size']) !!}>
     <option>Pequeno</option>
     <option>Médio</option>
     <option>Grande</option>
