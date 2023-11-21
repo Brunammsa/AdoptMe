733d86727427d9693800020a19c097efb5cdbc7d
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-21 16:11:43 -0300

    retirando asset de validação e trocando por if

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,38 +1,36 @@
 <form action="{{$action}}" class="form-create" method="POST">
     @csrf
-    @isset($name)
+    @if($update)
         @method('put')
-    @endisset
+    @endif
     
     <div>
         <x-input-label for="name" :value="__('Nome')" />
         <x-text-input id="name" class="block mt-1 w-full mb-4" 
-        type="text" 
         name="name" 
         required autofocus autocomplete="name"/>
+        <x-input-error :messages="$errors->get('password')" class="mt-2"/>
     </div>
     <div>
         <x-input-label for="age" :value="__('Idade')" />
         <x-text-input id="age" class="block mt-1 w-full mb-4" 
-        type="text" 
         name="age" 
         required autofocus autocomplete="age"/>
     </div>
     <div>
         <x-input-label for="size" :value="__('Porte')" />
-        <x-input-select class="block mt-1 w-25 mb-4"/>
+        <x-input-select class="block mt-1 w-25 mb-4" name="size" id="size" type="select"/>
     </div>
     <div>
         <x-input-label for="description" :value="__('Descreva seu pet')" />
-        <x-text-input id="age" class="block mt-1 w-full mb-4" 
-        type="text" 
+        <x-text-input id="description" class="block mt-1 w-full mb-4" 
         name="description" 
         required autofocus autocomplete="description"/>
     </div>
     
     <div>
-        <x-input-label for="size" :value="__('Onde está o pet?')" />
-        <x-select-cities class="block mt-1 w-25 mb-4"/>
+        <x-input-label for="locale" :value="__('Onde está o pet?')" />
+        <x-select-cities class="block mt-1 w-30 mb-4"/>
     </div>
     {{-- <div class="mb-3">
         <x-input-label for="formFileSm" :value="__('Foto')" />
