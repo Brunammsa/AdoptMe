3ee47703583a5d5ec756279ff33556b198caeecd
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-21 07:44:45 -0300

    retirei o @props()

--- resources/views/meusPets/create.blade.php
+++ resources/views/meusPets/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <section class="section-create">
         <div class="cabecalho-novoPet">
-            <h1 class=" mb-4 mt-20">Cadastro do pet:</h1>
+            <h1>Cadastro do pet:</h1>
             <img src="{{ asset('images/Patas.png') }}" alt="imagem patas" class="patas">
         </div>
         <x-form :action="route('meusPets.store')"></x-form>
