959fad723de31b1932e1acc12722c6c991fb5618
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-21 18:51:19 -0300

    tentei usar, mas quebra com o create

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,6 +1,7 @@
-@props(['type' => 'text', 'name'])
+@props(['id', 'name', 'value', 'type' => 'text'])
 
-<input  
-    type="{{ $type }}"
+<input type="{{ $type }}"
+    id="{{ $id }}"
     name="{{ $name }}"
+    value="{{ isset($value) ? $value : '' }}"
     {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
