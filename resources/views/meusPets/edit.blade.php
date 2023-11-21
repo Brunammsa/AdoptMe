9e72f26c94e4c52d383485a105442fef45e07ef9
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-21 18:52:07 -0300

    retirei @props e chamando variáveis por pets

--- resources/views/meusPets/edit.blade.php
+++ resources/views/meusPets/edit.blade.php
@@ -1,25 +1,13 @@
-@props([
-    'name' => $pets->name,
-    'age' => $pets->age,
-    'description' => $pets->description,
-    'id' => $pets->id,
-    'size' => $pets->size,
-    'state' => $states,
-    'cities' => $cities
-])
-
 <x-app-layout>
     <section class="section-create">
         <div class="cabecalho-novoPet">
-            <h1>Edição do pet  <strong>'{{$name}}'</strong></h1>
+            <h1>Edição do pet  <strong>'{{$pets->name}}'</strong></h1>
             <img src="{{ asset('images/Patas.png') }}" alt="imagem patas" class="patas">
         </div>
-        <x-form :action="route('meusPets.update', $id)" 
-            :name="old('name')"
-            :age="old('age')"
-            :description="old('description')"
-            :states="$states"
-            :cities="$cities"
+        <x-form :action="route('meusPets.update', $pets->id)" 
+            :name="$pets->name"
+            :age="$pets->age"
+            :description="$pets->description"
             :update="true"
             ></x-form>
     </section>
