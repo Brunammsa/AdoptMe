c2b6d95fc4623cbf7ee0c66ab4c948227b46ea70
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-22 11:40:50 -0300

    adicionando class no h1 create e edit

--- resources/views/meusPets/create.blade.php
+++ resources/views/meusPets/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <section class="section-create">
         <div class="cabecalho-novoPet">
-            <h1>Cadastro do pet:</h1>
+            <h1 class="cabecalho-add-edit">Cadastro do pet:</h1>
             <img src="{{ asset('images/Patas.png') }}" alt="imagem patas" class="patas">
         </div>
         <x-form :action="route('meusPets.store')" :update="false"></x-form>
--- resources/views/meusPets/edit.blade.php
+++ resources/views/meusPets/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <section class="section-create">
         <div class="cabecalho-novoPet">
-            <h1>Edição do pet  <strong>'{{$pets->name}}'</strong></h1>
+            <h1 class="cabecalho-add-edit">Edição do pet  <strong>'{{$pets->name}}'</strong></h1>
             <img src="{{ asset('images/Patas.png') }}" alt="imagem patas" class="patas">
         </div>
         <x-form :action="route('meusPets.update', $pets->id)" 
