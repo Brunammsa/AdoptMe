d26669ecbef1a8ce0f68c9758a4b9e443a08c67e
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-22 17:19:49 -0300

    responsividade

--- resources/views/components/input-select.blade.php
+++ resources/views/components/input-select.blade.php
@@ -1,6 +1,6 @@
 @props([])
 
-<select  {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm', 
+<select  {!! $attributes->merge(['class' => 'border-gray-300 text-sm focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm', 
         'name' => 'size', 
         'id'=> 'size']) !!}>
     <option>Pequeno</option>
