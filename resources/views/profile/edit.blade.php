970ef08f6ac285bf3667c20c16b062caacb9ecb0
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-23 17:17:40 -0300

    retirando edição de nome

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -2,11 +2,11 @@
 
     <div class="perfil-app">
         <div class="max-w-2xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            {{-- <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
                 </div>
-            </div>
+            </div> --}}
 
             {{-- <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
