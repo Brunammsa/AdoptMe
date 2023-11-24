5df0a7d5c0698fa9763bdbefd4e3dfb9cfca2cf4
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-24 18:10:45 -0300

    consegui passar nome da cidade com abreviação do estado e tentando passar imagem do pet

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,32 +4,40 @@
         <div class="pets">
             <ul>
                 @foreach ($pets as $pet)
-                    <li>
-                        <div class="card mb-3" style="max-width: 540px;">
-                            <div class="row g-0">
-                                <div class="col-md-4">
-                                    <img src="..." class="img-fluid rounded-start" alt="foto pet">
-                                </div>
-                                <div class="col-md-8">
-                                    <div class="card-body">
-                                        <h5 class="card-title">{{$pet->name}}</h5>
-                                        <p class="card-text">{{$pet->age}}</p>
-                                        <p class="card-text">{{$pet->size}}</p>
-                                        <p class="card-text">{{$pet->description}}</p>
-                                        <p class="card-text">
-                                            {{-- <small class="text-body-secondary">{{$pet->cities->name}}/{{$pet->cities->states->abbreviation}}</small> --}}
-                                        </p>
-                                        <a> 
-                                            <small><img src="{{asset('images/iconeMensagem.png')}}" alt=""> Falar com o responsável<small>
-                                        </a>
+                    @foreach ($files as $file)
+                        <li>
+                            <div class="card mb-3" style="max-width: 540px;">
+                                <div class="row g-0">
+                                    <div class="col-md-4">
+                                        <tr>
+                                            <td><img src="{{ asset('upload/haru.png') }}" style="height: 100px; width: 100px"></td>
+                                        </tr>
+                                        </tbody>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <img src="" alt="">
+                                            <h5 class="card-title">{{$pet->name}}</h5>
+                                            <p class="card-text">{{$pet->age}}</p>
+                                            <p class="card-text">{{$pet->size}}</p>
+                                            <p class="card-text">{{$pet->description}}</p>
+                                            <small class="text-body-secondary">
+                                                {{ $pet->cities->name ?? 'Cidade não encontrada' }} /
+                                                {{ $pet->cities->states->abbreviation}}
+                                            </small>
+                                        </div>
+                                        <div>
+                                            <button><img src="{{asset('images/iconeMensagem.png')}}" alt=""><button>
+                                            <span> Falar com o responsável</span>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </li>
+                        </li>
+                    @endforeach
+
                 @endforeach
             </ul>
         </div>
-        {{ $pets->links() }}
     </section>
 </x-app-layout>
