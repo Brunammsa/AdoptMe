79c2afb2b8573f886987d71e10bf2ade1deb75f3
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-27 14:38:52 -0300

    ajustando tags, diminuindo divs

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,41 +1,26 @@
 <x-app-layout>
-    <section class="section-app">
-        <p class="phrase-app">Olá! Veja os amigos disponíveis para adoção!</p>
-        <div class="pets">
-            <ul>
-                @foreach ($pets as $pet)
-                    @php
-                        $petImage = $files::where('pets_id', $pet->id)->first()
-                    @endphp
-                        <li>
-                            <div class="card mb-3" style="max-width: 540px;">
-                                <div class="row g-0">
-                                    <div class="col-md-4">
-                                        <tr>
-                                            <td><img src="{{ asset('upload/' . $petImage->name_upload) }}" style="height: 100px; width: 100px"></td>
-                                        </tr>
-                                    </div>
-                                    <div class="col-md-8">
-                                        <div class="card-body">
-                                            <img src="" alt="">
-                                            <h5 class="card-title">{{$pet->name}}</h5>
-                                            <p class="card-text">{{$pet->age}}</p>
-                                            <p class="card-text">{{$pet->size}}</p>
-                                            <p class="card-text">{{$pet->description}}</p>
-                                            <small class="text-body-secondary">
-                                                {{ $pet->cities->name}}/{{ $pet->cities->states->abbreviation}}
-                                            </small>
-                                        </div>
-                                        <div>
-                                            <button><img src="{{asset('images/iconeMensagem.png')}}" alt=""><button>
-                                            <span> Falar com o responsável</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                @endforeach
-            </ul>
-        </div>
-    </section>
+    <p class="phrase-app">Olá! Veja os amigos disponíveis para adoção!</p>
+    <ul class="ul-app">
+        @foreach ($pets as $pet)
+            @php
+                $petImage = $files::where('pets_id', $pet->id)->first()
+            @endphp
+            <li class="container-pet-dashboard">
+                <img class="pet-image" src="{{ asset('upload/' . $petImage->name_upload) }}" style="height: 150px; width: 150px">
+                <small class="card-title">Nome: <strong>{{$pet->name}}</strong></small>
+                <small class="card-text">Idade: {{$pet->age}}</small>
+                <small class="card-text">Porte: {{$pet->size}}</small>
+                <small class="card-text">{{$pet->description}}</small>
+                <small class="text-body-secondary">
+                    {{ $pet->cities->name}}/{{ $pet->cities->states->abbreviation}}
+                </small>
+                <div class="button-responsavel">
+                    <button>
+                        <img src="{{asset('images/iconeMensagem.png')}}" alt="">
+                    <button>
+                    <small>Falar com o responsável</small>
+                </div>
+            </li>
+        @endforeach
+    </ul>
 </x-app-layout>
