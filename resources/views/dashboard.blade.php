11111156dc776d17d9eff5bf111827ebddeaeb52
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-11-28 16:58:53 -0300

    ajustando body dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,26 +1,28 @@
 <x-app-layout>
-    <p class="phrase-app">Olá! Veja os amigos disponíveis para adoção!</p>
-    <ul class="ul-app">
-        @foreach ($pets as $pet)
-            @php
-                $petImage = $files::where('pets_id', $pet->id)->first()
-            @endphp
-            <li class="container-pet-dashboard">
-                <img class="pet-image" src="{{ asset('upload/' . $petImage->name_upload) }}" style="height: 150px; width: 150px">
-                <small class="card-title">Nome: <strong>{{$pet->name}}</strong></small>
-                <small class="card-text">Idade: {{$pet->age}}</small>
-                <small class="card-text">Porte: {{$pet->size}}</small>
-                <small class="card-text">{{$pet->description}}</small>
-                <small class="text-body-secondary">
-                    {{ $pet->cities->name}}/{{ $pet->cities->states->abbreviation}}
-                </small>
-                <div class="button-responsavel">
-                    <button>
-                        <img src="{{asset('images/iconeMensagem.png')}}" alt="">
-                    <button>
-                    <small>Falar com o responsável</small>
-                </div>
-            </li>
-        @endforeach
-    </ul>
+    <div>
+        <p class="phrase-app">Olá! Veja os amigos disponíveis para adoção!</p>
+        <ul class="ul-app">
+            @foreach ($pets as $pet)
+                @php
+                    $petImage = $files::where('pets_id', $pet->id)->first()
+                @endphp
+                <li class="container-pet-dashboard">
+                    <img class="pet-image" src="{{ asset('upload/' . $petImage->name_upload) }}" style="height: 150px; width: 150px">
+                    <small class="card-title">Nome: <strong>{{$pet->name}}</strong></small>
+                    <small class="card-text">Idade: {{$pet->age}}</small>
+                    <small class="card-text">Porte: {{$pet->size}}</small>
+                    <small class="card-text">{{$pet->description}}</small>
+                    <small class="text-body-secondary">
+                        {{ $pet->cities->name}}/{{ $pet->cities->states->abbreviation}}
+                    </small>
+                    <div class="button-responsavel">
+                        <button>
+                            <img src="{{asset('images/iconeMensagem.png')}}" alt="">
+                        <button>
+                        <small>Falar com o responsável</small>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 </x-app-layout>
