8bab5b86d8bd1efe6a53239aa0363f9db56f4d34
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-12-04 11:35:54 -0300

    dashboard com todos os pets

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,18 +10,20 @@
                     <div class="pet-image">
                         <img class="img-pets" src="{{ asset('upload/' . $petImage->name_upload) }}">
                     </div>
-                    <small class="card-title">Nome: <strong>{{$pet->name}}</strong></small>
-                    <small class="card-text">Idade: {{$pet->age}}</small>
-                    <small class="card-text">Porte: {{$pet->size}}</small>
-                    <small class="card-text">{{$pet->description}}</small>
-                    <small class="text-body-secondary">
-                        {{ $pet->cities->name}}/{{ $pet->cities->states->abbreviation}}
-                    </small>
-                    <div class="button-responsavel">
-                        <button>
-                            <img src="{{asset('images/iconeMensagem.png')}}" alt="">
-                        <button>
-                        <small>Falar com o responsável</small>
+                    <div class="card-text">
+                        <p class="card-name"><strong>{{$pet->name}}</strong></p>
+                        <small>{{$pet->age}}</small>
+                        <small>Porte {{$pet->size}}</small>
+                        <small>{{$pet->description}}</small>
+                        <small class="text-body-secondary">
+                            {{ $pet->cities->name}} ({{ $pet->cities->states->abbreviation}})
+                        </small>
+                        <div class="button-responsavel">
+                            <a href="#">
+                                <img src="{{asset('images/iconeMensagem.png')}}" alt="">
+                            </a>
+                            <small>Falar com o responsável</small>
+                        </div>
                     </div>
                 </li>
             @endforeach
