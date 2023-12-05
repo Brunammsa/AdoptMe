1fec3ef979619c3540ed14eaef650aab883815fb
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-12-05 11:07:13 -0300

    retirando div inutil e colocando rota na mensagem

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,25 +1,24 @@
 <x-app-layout>
-    <div>
-        <p class="phrase-app">Olá! Veja os amigos disponíveis para adoção!</p>
-        <ul class="ul-app">
-            @foreach ($pets as $pet)
-                @php
-                    $petImage = $files::where('pets_id', $pet->id)->first()
-                @endphp
-                <li class="container-pet-dashboard">
-                    <div class="pet-image">
-                        <img class="img-pets" src="{{ asset('upload/' . $petImage->name_upload) }}">
-                    </div>
-                    <div class="card-text">
-                        <p class="card-name"><strong>{{$pet->name}}</strong></p>
-                        <small>{{$pet->age}}</small>
-                        <small>Porte {{$pet->size}}</small>
-                        <small>{{$pet->description}}</small>
-                        <small class="text-body-secondary">
-                            {{ $pet->cities->name}} ({{ $pet->cities->states->abbreviation}})
-                        </small>
-                        <div class="button-responsavel">
-                            <a href="#">
+    <p class="phrase-app">Olá! Veja os amigos disponíveis para adoção!</p>
+    <ul class="ul-app">
+        @foreach ($pets as $pet)
+            @php
+                $petImage = $files::where('pets_id', $pet->id)->first()
+            @endphp
+            <li class="container-pet-dashboard">
+                <div class="pet-image">
+                    <img class="img-pets" src="{{ asset('upload/' . $petImage->name_upload) }}">
+                </div>
+                <div class="card-text">
+                    <p class="card-name"><strong>{{$pet->name}}</strong></p>
+                    <small>{{$pet->age}}</small>
+                    <small>Porte {{$pet->size}}</small>
+                    <small>{{$pet->description}}</small>
+                    <small class="text-body-secondary">
+                        {{ $pet->cities->name}} ({{ $pet->cities->states->abbreviation}})
+                    </small>
+                    <div class="button-responsavel">
+                            <a href="{{route('meusPets.contact')}}">
                                 <img src="{{asset('images/iconeMensagem.png')}}" alt="">
                             </a>
                             <small class="msg-dashboard">Falar com o responsável</small>
@@ -28,5 +27,4 @@
                 </li>
             @endforeach
         </ul>
-    </div>
 </x-app-layout>
