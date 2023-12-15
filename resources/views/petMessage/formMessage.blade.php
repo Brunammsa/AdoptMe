14434b0dd55102bbaf62ce8ef22122931b8ab473
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-12-15 17:25:52 -0300

    rota do controller mail e name e id message

--- resources/views/petMessage/formMessage.blade.php
+++ resources/views/petMessage/formMessage.blade.php
@@ -2,11 +2,11 @@
     <div class="container-message">
         <p class="p-message">Envie uma mensagem para a pessoa ou instituição que está cuidando do animal:</p>
         <section class="section-message">
-            <form action="{{route('meusPets.contactForm')}}" class="contact-form" method="POST">
+            <form action="{{route('mensagem.store')}}" class="contact-form" method="POST">
                 @csrf
                 <div class="box-message">
                     <label for="mensagem" class="label-mensagem">Mensagem</label>
-                    <textarea type="text" autofocus placeholder="Escreva sua mensagem" class="text-area border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"></textarea>
+                    <textarea type="text" name="message" id="message" autofocus placeholder="Escreva sua mensagem" class="text-area border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"></textarea>
                 </div>
                 <div class="buttons-message">
                     <button class="button-enviar-msg" type="submit" style="background-color: rgb(252, 112, 113)">Enviar</button>
